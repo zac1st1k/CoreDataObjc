cbf9c4a9ae875b3c1b9725e62fa3f6ce3b9e0eb1
zac1st1k <zacfirst@gmail.com>
2015-02-12 23:36:08 +1100

    Initial Commit

---
+++ CoreDataObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataObjc
+//
+//  Created by Zac on 12/02/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataObjc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataObjc
+//
+//  Created by Zac on 12/02/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
